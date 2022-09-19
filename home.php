7238207cf6fc49657c9875abead4cbe371415e00
pattaraporn <pattaraporn.w@psu.ac.th>
2022-09-19 08:13:55 +0700

    add favicon

--- home.php
+++ home.php
@@ -16,6 +16,7 @@
         rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
         integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link href="images/favicon.ico" rel="icon" type="image/x-icon" />
     <link rel="stylesheet" href="css/style.css">
 
     <title>MountainHigh: shoe shop</title>
